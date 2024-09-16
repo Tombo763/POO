8f223718ec89264bd42b41af89f1e905af1a2f1f
Tombo763 <olivaresalessandro10@gmail.com>
2024-09-15 23:46:41 -0300

    Subclase agregada

--- Iniciar clase (constructor).py
+++ Iniciar clase (constructor).py
@@ -8,3 +8,18 @@ class Auto: #Es el modelo o la idea de nuestro objeto
     
     def obtener_modelo(self):
         return self.__modelo
+    
+    def arrancar(self):
+        print(f"El {self.__marca} {self.__modelo} está arrancando.")
+
+class AutoDeportivo(Auto):
+    def __init__(self,marca,modelo,velocidad_maxima):
+        super().__init__(marca,modelo)
+        self.__velocidad_maxima = velocidad_maxima
+    
+    def obtener_velocidad_maxima(self):
+        return self.__velocidad_maxima
+    
+    def arrancar(self):
+        super().arrancar()
+        print(f"El {self.obtener_marca()} {self.obtener_modelo()} \ está listo para correr a una velocidad de {self.__velocidad_maxima} km/h.")
