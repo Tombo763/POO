cfb003586b427f29f504f0a9a23a481c5de2e6ce
Tombo763 <olivaresalessandro10@gmail.com>
2024-10-06 19:43:42 -0300

    sadasd

--- Biblioteca/Clases (Objetos)/probando.py
+++ Biblioteca/Clases (Objetos)/probando.py
@@ -1,7 +1,23 @@
-class Categoria:
+class Categoria: #CRUD
     def __init__(self, id_categoria, nombre):
         self.id_categoria = id_categoria
         self.nombre = nombre
+    
+    def crearUsuario(self):
+        print('Creando usuario...')
+        pass
+
+    def modificarUsuario(self):
+        print('Modificando usuario...')
+        pass
+
+    def seleccionarUsuario(self):
+        print('Seleccionando usuario...')
+        pass
+
+    def borrarUsuario(self):
+        print('Borrando usuario...')
+        pass
 
 class Libro:
     def __init__(self, ISBN, autor, editorial, anoPublicacion, categoria):
